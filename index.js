af9dd1f10125ae6e1e5b713cb8917452c72af3e7
danielshires <hello@danielshires.com>
2022-05-08 06:37:02 +1000

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const array = ['Lisa', 'Kaitlin', 'Jan']
+
+function writeCards(array, eventName) {
+    let arrayReturn = []
+    for (let i = 0; i < array.length; i++) {
+        arrayReturn.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return arrayReturn
+}
+
+console.log(writeCards(array, "surprise"))
+
+function countDown(num) {
+    let i = 0
+    while (i <= num) {
+        console.log(num)
+        num -= 1
+    }
+}
+
+countDown(10)
